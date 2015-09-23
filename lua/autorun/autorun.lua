a48cef7e2f66f420a95a17e4b3e2061cccaa62cc
Grim3212 <dakotapierone@yahoo.com>
2015-09-23 11:09:51 -0700

    Fix function returned in AddColumn
    Fixes the errors that AddColumn was returning a function not a string as
    well as setup all the UserGroups.

--- lua/autorun/autorun.lua
+++ lua/autorun/autorun.lua
@@ -1,19 +1,28 @@
---Add different things that are needed
+--Modify the scoreboard using TTTScoreboardColumns hook
 
 print("Grim3212 Initializing Scoreboard Ranks")
 
-local function setupRankColumn(pnl)
-	pnl:AddColumn("Rank", function(ply, label) 
-	
-	if ply:IsUserGroup("superadmin") then
+local function getRankAndColor(ply, label)
+	--Determine rank based on UserGroup
+	if ply:IsUserGroup( "superadmin" ) then
 		label:SetTextColor(COLOR_BLUE)
 		return "Owner"
-	elseif ply:IsUserGroup("admin") then
-		label:SetTextColor(COLOR_GREEN)
+	elseif ply:IsUserGroup( "admin" ) then
+		label:SetTextColor(COLOR_RED)
 		return "Admin"
-	else 
-		return "Guest"
-	end)
+	elseif ply:IsUserGroup( "moderator" ) then
+		label:SetTextColor(COLOR_GREEN)
+		return "Moderator"
+	elseif ply:IsUserGroup( "trusted" ) then
+		label:SetTextColor(COLOR_PURPLE)
+		return "Trusted"
+	else
+		return ""
+	end
+end
+
+local function setupRankColumn(pnl)
+	pnl:AddColumn("Rank", function(ply, label) return getRankAndColor(ply, label) end )
 end
 
 hook.Add( "TTTScoreboardColumns", "scoreboardRanks", setupRankColumn )
